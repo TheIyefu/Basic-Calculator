17a00423710e8bf8f5b7d52dbce383c1d8d8e235
TheIyefu <91689606+TheIyefu@users.noreply.github.com>
2022-05-20 10:35:56 -0700

    First commit

---
+++ calculator.js
@@ -0,0 +1,94 @@
+var screen = document.getElementById('display').textContent;
+var smallScreen = document.getElementById('small-screen').textContent;
+const point = document.getElementById('point');
+const equal = document.getElementById('equal');
+const numbers = document.querySelectorAll('.number');
+const signs = document.querySelectorAll('.signs');
+const erasers = document.querySelectorAll('.clear');
+
+
+function appendNumberClicked(e){
+    e.preventDefault();
+    var number = e.target.textContent;
+    if(screen === '0'){
+        screen = number;
+        document.getElementById('display').textContent = screen;
+        console.log(screen)
+    }
+    else{
+        screen = `${screen}${number}`;
+        document.getElementById('display').textContent = screen;
+        console.log(screen)
+    }
+}
+function addDecimalPoint(){
+    decimalPoint = point.textContent;
+    if(screen.includes(decimalPoint) == false){
+        screen += decimalPoint;
+        document.getElementById('display').textContent = screen;
+    }
+}
+function Operate(e){
+    e.preventDefault();
+    sign = e.target.textContent;
+    
+ 
+    if(sign == '×'){
+        sign = '*';
+    }
+    else if(sign == '÷'){
+        sign = '/';
+    }
+
+
+    if(smallScreen == 0){
+        smallScreen = `${screen}${sign}`;
+    }
+    else{
+        smallScreen += screen;
+        smallScreen = eval(smallScreen)
+        smallScreen += sign
+    }
+    screen = '0';
+    document.getElementById('small-screen').textContent = smallScreen;
+    document.getElementById('display').textContent = screen;
+}
+
+function Clear(e){
+    if (e.target.textContent == 'AC'){
+        smallScreen = '0'
+        document.getElementById('small-screen').textContent = smallScreen;
+    }
+    screen = '0'
+    document.getElementById('display').textContent = screen;
+}
+
+function displayResult(){
+    screen = eval(`${smallScreen}${screen}`)
+    if (screen != 'Infinity'){
+        document.getElementById('display').textContent = screen;
+    }
+    else{
+        alert('MATHEMATICAL ERROR!')
+        screen = '0'
+        document.getElementById('display').textContent = screen;
+    }
+    smallScreen = '0'
+    document.getElementById('small-screen').textContent = smallScreen;
+}
+
+for(let i = 0; i < numbers.length; i += 1){
+    numbers[i].addEventListener('click', appendNumberClicked);
+}
+
+point.addEventListener('click', addDecimalPoint);
+
+for(let i = 0; i < signs.length; i += 1){
+    signs[i].addEventListener('click', Operate);
+}
+
+for(let i = 0; i < erasers.length; i += 1){
+    erasers[i].addEventListener('click', Clear);
+}
+
+equal.addEventListener('click', displayResult)
